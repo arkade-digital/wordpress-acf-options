c2c3c5e16904d3e8f96288e3079508c88bc9cdb5
James Hunt <jamesh@arkade.com.au>
2015-11-27 12:11:28 +1100

    Initial commit

---
+++ acf-basic-options-page.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+Plugin Name: ACF Basic Options Page
+Plugin URI: http://www.arkade.com.au
+Description: Easily add ACF Options page without theme modification
+Version: 1.0
+Author: Arkade Digital
+Author URI: http://www.arkade.com.au
+*/
+
+
+function basic_acf_add_options() {
+
+	acf_add_options_page('Site Settings');
+
+}
+
+
+add_action( 'wp_loaded', 'basic_acf_add_options' );
+
+?>
